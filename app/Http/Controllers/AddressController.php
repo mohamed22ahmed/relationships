a0ada85cb17854644b4ee670f2fb1410801fee50
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2022-10-02 19:41:47 +0200

    get the users who don't have any address

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -21,9 +21,13 @@ class AddressController extends Controller
         // here has gets the users who has address more than 2 addresses
         // $users = User::has('address', '>=', 2)->with('address')->get();
 
-        $users = User::whereHas('address', function($query){
-            $query->where('country', 'like', '%Egy%');
-        })->with('address')->get();
+        // get the user which has the address with substring 'Egy'
+        // $users = User::whereHas('address', function($query){
+        //     $query->where('country', 'like', '%Egy%');
+        // })->with('address')->get();
+
+        // get the users who don't have any address
+        // $users = User::doesntHave('address')->with('address')->get();
 
         return view('address', compact('users'));
     }
